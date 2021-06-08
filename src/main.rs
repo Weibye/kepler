d160cc01b8ab5fff37625bbc47bb400ebba27855
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2021-06-08 09:17:42 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use bevy::{pbr::AmbientLight, prelude::{App, ClearColor, Color, Msaa, WindowDescriptor}};
+use bevy::DefaultPlugins;
+use game_plugin::GamePlugin;
+
+fn main() {
+    App::build()
+        .insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 1.0 / 5.0f32,
+        })
+        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
+        .insert_resource(WindowDescriptor {
+            width: 800.,
+            height: 600.,
+            title: "Space Pilot".to_string(),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(GamePlugin)
+        .run();
+}
