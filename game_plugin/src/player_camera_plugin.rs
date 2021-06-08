794987802d810d26cdd4575095b3cf95ba3aa947
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2021-06-08 09:18:19 +0200

    Initial scaffolding on player plugin

---
+++ game_plugin/src/player_camera_plugin.rs
@@ -0,0 +1,12 @@
+pub struct PlayerCameraPlugin;
+
+impl Plugin for PlayerCameraPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+
+    }
+}
+
+// Spawn camera
+
+// translate camera when user presses WASD buttons
+
